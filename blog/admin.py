bdde3752106a8e4b6c1ca3452deb5d4f2de05635
SiyamDev <siyamdev705@gmail.com>
2026-02-05 16:51:23 +0600

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at', 'view_count')
+    list_filter = ('status', 'created_at', 'author')
+    search_fields = ('title', 'content', 'author__username')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('created_at', 'author')
+    search_fields = ('content', 'author__username', 'post__title')
+    raw_id_fields = ['author', 'post']
